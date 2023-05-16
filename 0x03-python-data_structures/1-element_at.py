cc9d609b59dbe3afc44185d685d59d81ee710fc0
GeoffreyOuma <bygeoffreyouma@gmail.com>
2023-05-16 03:41:49 -0700

    retrieves an element from a list like in C

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@
 # retrieves an element from a list
 
 def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list) - 1:
-        return none
+    if idx < 0 or idx > len(my_list):
+        return "None"
     else:
         return my_list[idx]
