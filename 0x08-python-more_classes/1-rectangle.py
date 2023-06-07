a3e2ff1994d337b06aa5baa437a75fe2b515fb72
thinkingemu <bygeoffreyouma@gmail.com>
2023-06-07 10:36:53 -0700

    update 1-rectangle.py file

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -26,7 +26,7 @@ class Rectangle:
     def width(self, value):
         """To protect width from receiving invalid values"""
 
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -43,7 +43,7 @@ class Rectangle:
     def height(self, value):
         """To protect height from receiving invalid values"""
 
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
