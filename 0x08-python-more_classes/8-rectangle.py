9763310c3484fb9767b4bb79cb4585f50c6ccae7
thinkingemu <bygeoffreyouma@gmail.com>
2023-06-07 12:33:32 -0700

    a class Rectangle that defines a rectangle

---
+++ 0x08-python-more_classes/8-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""
+Module
+"""
+
+class Rectangle:
+    """
+    Defines class rectangle
+    """
+
+    number_of_instances = 0
+    print_simple = "#"
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """Static method"""
+        if type(rect_1) is not Rectangle:
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if type(rect_2) is not Rectangle:
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        else:
+            return rect_2
+
+    def __init__(self, width=0, height=0):
+        """
+        Attribs
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Protecting width from accepting invalid values"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return area of the rect"""
+        return self.height * self.width
