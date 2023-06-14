47ab7b9c9436a168e1e08ce2a4a9b44db70e7096
thinkingemu <bygeoffreyouma@gmail.com>
2023-06-14 06:25:27 -0700

    a function that creates an Object from a “JSON file”

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module for function: load_from_json_file
+"""
+
+import json
+
+def load_from_json_file(filename):
+    """
+    function tht creates an Objects from a "JSON file"
+    """
+    with open(filename, 'r', encoding='utf-8') as fi_le:
+        return json.load(fi_le)
