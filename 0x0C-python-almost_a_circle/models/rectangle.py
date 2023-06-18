237a26078f504b5574a6fc64cd25e7d3db3d95b7
Fribuz <bygeofreyouma@gmail.com>
2023-06-18 07:31:49 -0700

    rect

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+class Rectangle
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """
+    class rectangle that inherits Base
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be greater than 0")
+        self.__width = value
