545a4f58637dcf819b400dbe731e0dcbdc0eed23
i18nfoo <bygeoffreyouma@gmail.com>
2023-07-19 15:09:56 -0700

    Write a script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on MySQL server

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,14 @@
+-- creates the database hbtn_0d_usa and the table cities(in database hbtn_0d_usa) on MySQL server
+-- 	cities description:
+-- 		id INT unique, auto generated, can’t be null and is a primary key
+-- 		state_id INT, can’t be null and must be a FOREIGN KEY that references to id of the states table
+-- 		name VARCHAR(256) can’t be null
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+use hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS `cities`
+(
+	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	FOREIGN KEY(state_id) REFERENCES state(id)
+);
