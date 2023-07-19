e0c6876fd002ce9058be96a0a95694a213d4d538
i18nfoo <bygeoffreyouma@gmail.com>
2023-07-19 16:12:59 -0700

    Write a script that lists all cities contained in the database hbtn_0d_usa

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,8 @@
+-- lists all cities contained in the database hbtn_0d_usa
+-- 	Each record should display: cities.id - cities.name - states.name
+-- 	Results must be sorted in ascending order by cities.id
+SELECT a.id AS id a.name AS name, b.name AS name
+FROM cities a
+INNER JOIN states b
+ON a.state_id = b.id
+ORDER BY a.id ASC;
