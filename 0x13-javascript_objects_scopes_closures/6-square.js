f479631d74893072c88c2fcd6a6a974f090326ff
betamodule <bygeoffreyouma@gmail.com>
2023-08-15 23:32:26 +0000

    Write a class Square that defines a square and inherits from Square of 5-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const SquareOne = require('./5-square');
+module.exports = class Square extends SquareOne {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log('c'.repeat(this.width));
+      }
+    }
+  }
+};
