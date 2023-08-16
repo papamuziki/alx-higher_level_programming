a75dfc0db9b88866cfd6eba73c477425346ceb1b
betamodule <bygeoffreyouma@gmail.com>
2023-08-16 21:42:26 +0000

    Write a script that prints the first argument passed to it

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
 
-const process = require('process');
-const argv = process.argv;
-
-for (let i = 0; i < argv - 1; i++){
-	if (argv === null){
-		console.log('No argument');
-	}
-	console.log(argv[0]);
+if (process.argv[2] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
 }
