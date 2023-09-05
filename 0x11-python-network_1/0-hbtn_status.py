def7e93e7d553bb79b1d39815afcd4f913a6cbbe
papasnoopie <bygeoffreyouma@gmail.com>
2023-09-05 11:52:43 -0700

    Write a Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""This script fetches https://alx-intranet.hbtn.io/status
+    - using urllib package
+    - and a with statement
+"""
 
-import urllib.request
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    html = response.read()
+if __name__ == "__main__":
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        content = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(content)))
+        print('\t- content: {}'.format(content))
+        print('\t- utf-8 content: {}'.format(content.decode('utf-8')))
