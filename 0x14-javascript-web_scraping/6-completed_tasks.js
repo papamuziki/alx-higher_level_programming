8f021bd3364dd749942ab6436c5feb94cbb9c0bd
papaskripto <bygeoffreyouma@gmail.com>
2023-10-06 14:30:38 -0700

    Task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,21 +3,21 @@
 const request = require('request');
 const apiurl = process.argv[2];
 
-request(apiurl, function(err, response, body){
-  if (err){
+request.get(apiurl, function (err, response, body) {
+  if (err) {
     console.log(err);
-  } else if (response.statusCode === 200){
+  } else if (response.statusCode === 200) {
     const completedTasks = {};
     const tasks = JSON.parse(body);
-  
-    for (let i in tasks){
+
+    for (const i in tasks) {
       const task = tasks[i];
-      if (task.completedTasks === true){
-        if (completedTasks[task.userId] == undefined){
+      if (task.completedTasks === true) {
+        if (completedTasks[task.userId] == undefined) {
 	  completedTasks[task.userId] = 1;
-	} else {
+        } else {
 	  completedTasks[task.userId]++;
-	}
+        }
       }
     }
     console.log(completedTasks);
