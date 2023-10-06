19c77a5bd688b42cc733e09a2c259a18b7aa513e
papaskripto <bygeoffreyouma@gmail.com>
2023-10-06 13:36:34 -0700

    Movie char

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+// prints all characters of a Star Wars movie
+
+const req = require('request');
+const movieId = process.argv[2];
+const swapi = 'https://swapi-api.alx-tools.com/api/people/';
+
+function starWarsMovieChars(movieId, swapi){
+  req(swapi, function (err, body, response){
+    if (err) {
+      console.log(err);
+    }
+    const jsonObject = JSON.parse(body);
+    const people = jsonObject.results;
+    for (let i in people) {
+      for (j in people[i].films){
+        if (people[i].films[j].includes(movie)){
+        console.log(people[i].name);
+        }
+      }
+    }
+    if (jsonObject.next !== null){
+      starWarsMovieChars(movieId, jsonObject.next);
+    }
+  });
+}
+starWarsMovieChars(movieId, swapi);
