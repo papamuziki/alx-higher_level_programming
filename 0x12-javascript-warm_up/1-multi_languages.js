3e7733793b6379f0a1ef9af2f80702e9512d4b21
papaskripto <bygeoffreyouma@gmail.com>
2024-03-16 21:01:53 +0300

    A script that prints 3 lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
+/*
 console.log('C is fun');
 console.log('Python is cool');
 console.log('JavaScript is amazing');
+*/
+// Rewrite above code in one line
+console.log("C is fun\nPython is cool\nJavaScript is amazing");
