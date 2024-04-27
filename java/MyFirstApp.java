0f604001f37640d456b733e4daee46f3813ba907
papaskripto <bygeoffreyouma@gmail.com>
2024-04-27 15:05:07 +0300

    I rule the programming world

---
+++ java/MyFirstApp.java
@@ -0,0 +1,12 @@
+public class MyFirstApp {
+	public static void main(String[] args) {
+		System.out.println("I Rule!");
+		System.out.println("the programming world");
+	}
+}
+
+/* 
+ * Save: MyFirstApp.java
+ * Compile: javac MyFirstApp.java
+ * Run: java MyFirstApp
+ */
