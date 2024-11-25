294727e22e12e5c5d7f58535dbab2c38cf74ccc1
papamuziki <bygeoffreyouma@gmail.com>
2024-11-25 03:02:54 +0300

    print integer, use str.format()

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,7 @@
-#!/usr/bin/python3
+#! /usr/bin/python3
+
+# print
+# number, followed by, Battery street
+
 number = 98
-print(f"{number:d} Battery street")
+print ('{} Battery street'.format (number))
