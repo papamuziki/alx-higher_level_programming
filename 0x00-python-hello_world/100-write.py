07b0bb3486436d198a75212bc220d55590d1c2d9
papamuziki <bygeoffreyouma@gmail.com>
2024-11-25 03:33:52 +0300

    now readable

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,4 +1,11 @@
-#!/usr/bin/python3
+#! /usr/bin/python3
+
+# print using write instead of print ()
+# in stderr mode and exit code 1
+# followed by new line
+
 import sys
-sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
-exit(1)
+
+
+sys.stderr.write ('and that piece of art is useful - Dora Korpar, 2015-10-19\n')
+exit (1)
